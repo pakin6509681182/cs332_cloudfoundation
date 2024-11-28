e0cefda98d0c04eb0b5c2d1fbbb01bfa48d9ab67
pakin6509681182 <pakin.pra@dome.tu.ac.th>
2024-11-28 15:20:47 +0000

    Change logout flash message type from 'success' to 'info' for better clarity

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ def signup_success():
 @app.route('/logout')
 def logout():
     session.clear()
-    #flash('You have been logged out.', 'success')
+    flash('You have been logged out.', 'info')
     return redirect(url_for('login'))
 
 @app.route('/details_camera')
